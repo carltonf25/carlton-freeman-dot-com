a54fd5f413e31e3065b88580b5290205e92f13aa
carltonf25 <carltonfreeman24@gmailcom>
2019-09-24 12:56:21 -0400

    slide-y button animations, oooo

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import {useSpring, animated} from 'react-spring';
 
-const BackToTopButton = () => (
-    <button style={{
-        fontSize: `12px`,
-        border: `none`,
-        color: `#fefefe`,
-        background: `#3c2d50`,         
-        boxShadow: `0 4px 6px rgba(100, 100, 100, 0.2)`,
-        position: `fixed`,
-        bottom: `3em`,
-        right: `3em`,
-        padding: `1em 1em`,
-        textTransform: `uppercase`,
-        fontWeight: 600,
-        cursor: `pointer`,
+const BackToTopButton = () => {
+    const slideIn = useSpring({
+        from: {
+          transform: `translate3d( 300px, 0, 0)`
+        },
+        to: {
+          transform: `translate3d(0,0,0)`
+        }
+      }) 
+    return (
+        <animated.button style={{ ...slideIn,  
+            fontSize: `12px`,
+            border: `none`,
+            color: `#fefefe`,
+            background: `#3c2d50`,         
+            boxShadow: `0 4px 6px rgba(100, 100, 100, 0.2)`,
+            position: `fixed`,
+            bottom: `3em`,
+            right: `3em`,
+            padding: `1em 1em`,
+            textTransform: `uppercase`,
+            fontWeight: 600,
+            cursor: `pointer`,
+        }}
+        onClick={() => window.scrollTo({
+            top: `0`,
+            behavior: 'smooth'
+          })}>
+            Back to Top <span style={{fontSize: `18px`}}>☝</span>️
+        </animated.button>
+    );
+}
 
-    }}
-    onClick={() => window.scrollTo({
-        top: `0`,
-        behavior: 'smooth'
-      })}>
-        Back to Top <span style={{fontSize: `18px`}}>☝</span>️
-    </button>
-);
 
 export default BackToTopButton;
